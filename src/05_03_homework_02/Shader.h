5b27a663163d2e155beeb764072a01e46dcd98f5
mimizao <88256191@qq.com>
2023-06-29 00:59:13 +0800

    05_03_homework_02

---
+++ src/05_03_homework_02/Shader.h
@@ -0,0 +1,51 @@
+#ifndef INC_03_SHADERS_SHADER_H
+#define INC_03_SHADERS_SHADER_H
+
+#include "glm/detail/qualifier.hpp"
+#include "glm/fwd.hpp"
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+public:
+    //! 程序ID
+    unsigned int ID;
+    //! 构造器读取并构建着色器
+    Shader(const char *vertexPath, const char *fragmentPath);
+    //! 使用/激活程序
+    void use() const;
+    //! uniform工具函数
+    [[maybe_unused]]
+    void setBool(const std::string &name, bool value) const;
+    [[maybe_unused]]
+    void setInt(const std::string &name, int value) const;
+    [[maybe_unused]]
+    void setFloat(const std::string &name, float value) const;
+    [[maybe_unused]]
+    void setVec2(const std::string &name, const glm::vec2 &value) const;
+    [[maybe_unused]]
+    void setVec2(const std::string &name, float x, float y) const;
+    [[maybe_unused]]
+    void setVec3(const std::string &name, const glm::vec3 &value) const;
+    [[maybe_unused]]
+    void setVec3(const std::string &name, float x, float y, float z) const;
+    [[maybe_unused]]
+    void setVec4(const std::string &name, const glm::vec4 &value) const;
+    [[maybe_unused]]
+    void setVec4(const std::string &name, float x, float y, float z, float w) const;
+    [[maybe_unused]]
+    void setMat2(const std::string &name, const glm::mat2 &mat) const;
+    [[maybe_unused]]
+    void setMat3(const std::string &name, const glm::mat3 &mat) const;
+    [[maybe_unused]]
+    void setMat4(const std::string &name, const glm::mat4 &mat) const;
+private:
+    static void checkCompileErrors(unsigned int shader, const std::string &type);
+};
+
+#endif //INC_03_SHADERS_SHADER_H
