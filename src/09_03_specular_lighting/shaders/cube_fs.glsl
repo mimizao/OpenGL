3233f44ebbe7c3d9acd4aa5e3266ce595f3ea356
mimizao <88256191@qq.com>
2023-07-10 00:28:43 +0800

    09_03_specular_lightingm

---
+++ src/09_03_specular_lighting/shaders/cube_fs.glsl
@@ -0,0 +1,33 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec3 FragPos;
+in vec3 Normal;
+
+uniform vec3 cubeColor;
+uniform vec3 lampColor;
+uniform vec3 lampPos;
+uniform vec3 viewPos;
+
+void main()
+{
+    //! ambient
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lampColor;
+
+    //! diffuse
+    vec3 norm = normalize(Normal);
+    vec3 lampDir = normalize(lampPos - FragPos);
+    float diff = max(dot(norm, lampDir), 0.0);
+    vec3 diffuse = diff * lampColor;
+
+    //! specular
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lampDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 256);
+    vec3 specular = specularStrength * spec * lampPos;
+
+    vec3 result = (ambient + diffuse + specular) * cubeColor;
+    FragColor = vec4(result, 1.0);
+}
